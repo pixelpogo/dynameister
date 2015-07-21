b6ac94163bfece2b886f6fc46f9c989d00f1d4c2
Sebastian Oelke <soelke@babbel.com>
2015-07-21 13:09:40 +0200

    Adds configuration to dynameister.

--- lib/dynameister.rb
+++ lib/dynameister.rb
@@ -1,5 +1,41 @@
 require "dynameister/version"
 
+require "dynameister/client"
+
 module Dynameister
+
+  def self.configure(&block)
+    @config = Dynameister::Config.new
+    yield @config
+  end
+
+  def self.config
+    @config
+  end
+
+  class Config
+
+    def initialize
+      @read_capacity  = 1
+      @write_capacity = 1
+    end
+
+    def read_capacity(read_capacity = nil)
+      if read_capacity
+        @read_capacity = read_capacity
+      else
+        @read_capacity
+      end
+    end
+
+    def write_capacity(write_capacity = nil)
+      if write_capacity
+        @write_capacity = write_capacity
+      else
+        @write_capacity
+      end
+    end
+
+  end
 
 end
---
+++ spec/dynameister_spec.rb
@@ -0,0 +1,19 @@
+require "spec_helper"
+
+describe Dynameister do
+
+  let(:capacity) { 99 }
+
+  before :each do
+    Dynameister.configure do |config|
+      config.read_capacity  capacity
+      config.write_capacity capacity
+    end
+  end
+
+  subject { Dynameister.config }
+
+  its(:read_capacity)  { is_expected.to eq(capacity) }
+  its(:write_capacity) { is_expected.to eq(capacity) }
+
+end
