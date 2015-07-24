18e5b6147754e29456e41b6d9c4db8a2f7c3954d
Sebastian Oelke <soelke@babbel.com>
2015-07-24 11:32:33 +0200

    Shortened tests.

--- spec/dynameister/serializers/get_item_serializer_spec.rb
+++ spec/dynameister/serializers/get_item_serializer_spec.rb
@@ -2,6 +2,18 @@ require_relative "../../../lib/dynameister/serializers/get_item_serializer.rb"
 
 describe Dynameister::Serializers::GetItemSerializer do
 
+  shared_examples_for "a serializer that includes table name and hash key" do
+
+    it "includes table_name" do
+      expect(subject).to include(table_name: table_name)
+    end
+
+    it "includes the hash_key" do
+      expect(subject[:key]).to include(hash_key)
+    end
+
+  end
+
   let(:table_name) { "my-table" }
   let(:hash_key)   { { id: 123 } }
 
@@ -11,26 +23,14 @@ describe Dynameister::Serializers::GetItemSerializer do
 
     subject { described_class.new(options).to_h }
 
-    it "includes table_name" do
-      expect(subject).to include(table_name: table_name)
-    end
-
-    it "includes the hash_key" do
-      expect(subject).to include(key: hash_key)
-    end
+    it_behaves_like "a serializer that includes table name and hash key"
 
     context "with an additional range key given" do
 
       let(:range_key) { { user: "john doe" } }
       let(:options)   { { table_name: table_name, hash_key: hash_key, range_key: range_key } }
 
-      it "includes table_name" do
-        expect(subject).to include(table_name: table_name)
-      end
-
-      it "includes the hash_key" do
-        expect(subject[:key]).to include(hash_key)
-      end
+      it_behaves_like "a serializer that includes table name and hash key"
 
       it "includes the range_key" do
         expect(subject[:key]).to include(range_key)
