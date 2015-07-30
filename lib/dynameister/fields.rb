cd629ebaef86bdc8337a7c1bcd6d8798927f26c4
Sebastian Oelke <soelke@babbel.com>
2015-07-30 13:03:57 +0200

    Added method definition.

--- lib/dynameister/fields.rb
+++ lib/dynameister/fields.rb
@@ -4,21 +4,15 @@ module Dynameister
 
     included do
       class_attribute :attributes
-
       self.attributes = {}
-
-      field :id # default primary key
     end
 
     module ClassMethods
 
       def field(name, type = :string, options = {})
-        named = name.to_s
-        self.attributes = attributes.merge(name => {type: type}.merge(options))
+        method_name = name.to_s
 
-        define_method(named) { read_attribute(named) }
-        define_method("#{named}?") { !read_attribute(named).nil? }
-        define_method("#{named}=") {|value| write_attribute(named, value) }
+        define_method(method_name) { }
       end
 
     end
