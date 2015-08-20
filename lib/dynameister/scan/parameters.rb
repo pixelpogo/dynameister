fbf04e4206fd1b2c74a7c7046b46b77a5ebfcf05
Sebastian Oelke <soelke@babbel.com>
2015-08-20 12:08:56 +0200

    Refactoring 1.

--- lib/dynameister/scan/parameters.rb
+++ lib/dynameister/scan/parameters.rb
@@ -32,24 +32,34 @@ module Dynameister
       end
 
       def filter_expression
-        expression_attribute_names  = {}
-        expression_attribute_values = {}
-
-        @options.each do |key, value|
-          expression_attribute_names["##{key}"]  = key.to_s
-          expression_attribute_values[":#{key}"] = value
-        end
-
-        expression_attributes = Hash[expression_attribute_names.keys.zip(expression_attribute_values.keys)]
+        expression_attributes = build_expression_attributes
 
         {
           filter_expression:           build_filter_expression(expression_attributes),
-          expression_attribute_names:  expression_attribute_names,
-          expression_attribute_values: expression_attribute_values
+          expression_attribute_names:  expression_attributes[:names],
+          expression_attribute_values: expression_attributes[:values]
         }
       end
 
-      def build_filter_expression(expression_attributes)
+      def build_expression_attributes
+        initial = {
+          names:  {},
+          values: {}
+        }
+
+        @options.each_with_object(initial) do |(key, value), attributes|
+          attributes[:names]["##{key}"]  = key.to_s
+          attributes[:values][":#{key}"] = value
+        end
+      end
+
+      def build_filter_expression(raw_expression_attributes)
+        expression_attributes = Hash[
+          raw_expression_attributes[:names]
+            .keys
+            .zip(raw_expression_attributes[:values].keys)
+        ]
+
         expression_attributes.each_with_object([]) do |(name, value), filter|
           filter << "#{name} = #{value}"
         end.join(" AND ")
