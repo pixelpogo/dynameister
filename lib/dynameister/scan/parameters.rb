7e2870545bf48de3db11c5d37ec40e9aa07dc984
Sebastian Oelke <soelke@babbel.com>
2015-08-20 12:23:31 +0200

    Refactoring 3.

--- lib/dynameister/scan/parameters.rb
+++ lib/dynameister/scan/parameters.rb
@@ -16,6 +16,7 @@ module Dynameister
           index = find_local_index_for_attributes
           hash[:index_name] = index if index
         end
+
         params.merge!(filter_expression)
       end
 
@@ -33,9 +34,10 @@ module Dynameister
 
       def filter_expression
         expression_attributes = build_expression_attributes
+        filter_expression     = build_filter_expression(expression_attributes)
 
         {
-          filter_expression:           build_filter_expression(expression_attributes),
+          filter_expression:           filter_expression,
           expression_attribute_names:  expression_attributes[:names],
           expression_attribute_values: expression_attributes[:values]
         }
@@ -62,10 +64,10 @@ module Dynameister
       end
 
       def expression_attribute_key_mapping(expression_attributes)
-        attribute_names_keys   = expression_attributes[:names].keys
-        attrribute_values_keys = expression_attributes[:values].keys
+        attribute_names_keys  = expression_attributes[:names].keys
+        attribute_values_keys = expression_attributes[:values].keys
 
-        Hash[attribute_names_keys.zip(attrribute_values_keys)]
+        Hash[attribute_names_keys.zip(attribute_values_keys)]
       end
 
     end
