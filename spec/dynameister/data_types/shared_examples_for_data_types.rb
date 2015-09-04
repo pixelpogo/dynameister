ecba7a6e5e6de14ac786e0676b5623fe583d12cf
Oliver Grimm <olly@pixelpogo.de>
2015-09-04 15:46:20 +0200

    fix shared example description

--- spec/dynameister/data_types/shared_examples_for_data_types.rb
+++ spec/dynameister/data_types/shared_examples_for_data_types.rb
@@ -4,7 +4,7 @@ shared_examples_for "a DataType that can serialize and deserialize values in bot
     expect(subject.deserialize(subject.serialize(attribute))).to eq attribute
   end
 
-  it "serializes a deserialized value is the same as serializing the initial value" do
+  it "serializes a deserialized value into the same value as the initial serialized value" do
     expect(subject.serialize(subject.deserialize(subject.serialize(attribute)))).to eq subject.serialize(attribute)
   end
 
