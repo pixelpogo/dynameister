856eb6a0b293e5e6a5f4aa70f00c78ea9f5afa2f
Sebastian Oelke <soelke@babbel.com>
2015-10-01 16:29:05 +0200

    Fixed typo.

--- spec/app/models/cat.rb
+++ spec/app/models/cat.rb
@@ -4,12 +4,13 @@ class Cat
 
   field :pet_food
   field :adopted_at, :datetime
+  field :created_at
 
   # local indexes can only be defined on tables with hash and range key
   table name: "kittens", hash_key: :name, range_key: :created_at
 
   local_index :adopted_at # projection: :all, :keys_only, [:attr1, :attr2]
 
-  global_index [:pet_food, :adpoted_at], projection: :all, throughput: [2, 3]
+  global_index [:pet_food, :adopted_at], projection: :all, throughput: [2, 3]
 
 end
