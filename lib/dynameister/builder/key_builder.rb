87cb2950dc68f32b2f571b8ff7b0f6b64f41b22f
Sebastian Oelke <soelke@babbel.com>
2015-10-05 17:04:47 +0200

    Refactoring.

--- lib/dynameister/builder/key_builder.rb
+++ lib/dynameister/builder/key_builder.rb
@@ -9,22 +9,25 @@ module Dynameister
     class KeyBuilder
 
       def self.build_hash_key(hash_key)
-        case hash_key
-        when String, Symbol then Key.new(hash_key.to_sym, :string)
-        when Hash           then Key.new(hash_key.keys.first,
-                                         hash_key.values.first)
-        else raise IndexKeyDefinitionError.new(hash_key)
-        end
+        create_key hash_key, :hash
       end
 
       def self.build_range_key(range_key)
-        case range_key
-        when String, Symbol then Key.new(range_key.to_sym, :number)
-        when Hash           then Key.new(range_key.keys.first,
-                                         range_key.values.first)
-        else raise IndexKeyDefinitionError.new(range_key)
+        create_key range_key, :range
+      end
+
+      def self.create_key(key_information, key_type)
+        default_key_data_type = key_type == :hash ? :string : :number
+
+        case key_information
+        when String, Symbol then Key.new(key_information.to_sym, default_key_data_type)
+        when Hash           then Key.new(key_information.keys.first,
+                                         key_information.values.first)
+        else raise IndexKeyDefinitionError.new(key_information)
         end
       end
+
+      private_class_method :create_key
 
     end
 
