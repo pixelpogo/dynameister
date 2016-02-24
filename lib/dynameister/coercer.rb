58fbf7bfe1ffa5aaf67a4b68a7544c7c5786b7a8
Nicola Sheldrick <sheldr@gmail.com>
2016-02-24 12:33:42 +0100

    Moved aws data types mapping to appropiate module

--- lib/dynameister/coercer.rb
+++ lib/dynameister/coercer.rb
@@ -1,3 +1,4 @@
+require "dynameister/data_types"
 require_relative "errors"
 
 module Dynameister
@@ -5,18 +6,6 @@ module Dynameister
   class Coercer
 
     Key = Struct.new(:name, :type)
-
-    DEFAULT_AWS_DATA_TYPE = :string
-
-    AWS_DATA_TYPE = {
-      time:     :string,
-      datetime: :string,
-      integer:  :number,
-      float:    :number,
-      string:   :string,
-      number:   :number,
-      binary:   :binary
-    }.freeze
 
     attr_accessor :schema
 
@@ -29,7 +18,7 @@ module Dynameister
       when String, Symbol then Key.new(key_information.to_sym,
                                        to_aws_type(key_information))
       when Hash           then Key.new(key_information.keys.first,
-                                       AWS_DATA_TYPE[key_information.values.first])
+                                       map_aws_data_type(key_information.values.first))
       else raise KeyDefinitionError.new(key_information)
       end
     end
@@ -38,11 +27,19 @@ module Dynameister
 
     def to_aws_type(name)
       data_type = extract_type_from_schema(name)
-      AWS_DATA_TYPE[data_type]
+      map_aws_data_type(data_type)
     end
 
     def extract_type_from_schema(name)
-      schema[name] ? schema[name][:type] : DEFAULT_AWS_DATA_TYPE
+      if schema[name]
+        schema[name][:type]
+      else
+        DataTypes::DEFAULT_AWS_DATA_TYPE
+      end
+    end
+
+    def map_aws_data_type(type)
+      DataTypes::AWS_DATA_TYPE_MAP[type]
     end
 
   end
